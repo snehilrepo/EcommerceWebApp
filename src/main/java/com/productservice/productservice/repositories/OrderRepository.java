a97041a1018495f6405db15def5f3e535b6ce90b
Snehil Anand <snehilanand@Snehils-MacBook-Air.local>
2024-10-27 16:26:18 +0530

    Versioned migration

---
+++ src/main/java/com/productservice/productservice/repositories/OrderRepository.java
@@ -0,0 +1,4 @@
+package com.productservice.productservice.repositories;
+
+public interface OrderRepository extends org.springframework.data.jpa.repository.JpaRepository<com.productservice.productservice.models.Order, java.util.UUID> {
+  }
--- src/main/resources/db/migration/V2__somemodel_addValue.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE some_model
-    ADD value INT NULL;
-
-ALTER TABLE some_model
-    MODIFY value INT NOT NULL;
